a85e87cd24745f45c5ade3a5b57b50c5dc5d4938
tkyk.niimura <tkyk.niimura@gmail.com>
2020-05-17 21:01:20 +0900

    初期コミット

---
+++ index.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Hello = /** @class */ (function () {
+    function Hello() {
+    }
+    Hello.prototype.greeting = function (name) {
+        return "Hello" + name;
+    };
+    return Hello;
+}());
+exports.Hello = Hello;
---
+++ index.ts
@@ -0,0 +1,9 @@
+import h from 'hyperscript';
+
+export class Hello {
+    greeting (name :string){
+      return "Hello"+name;
+    }
+ }
+
+
